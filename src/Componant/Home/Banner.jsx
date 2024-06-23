5db4cb10f0a77227c424c00d05d57ab0ab307c9a
Osman <osmangoni0827@gmail.com>
2024-06-23 18:16:14 +0600

    update

--- src/Componant/Home/Banner.jsx
+++ src/Componant/Home/Banner.jsx
@@ -11,88 +11,94 @@ import Typical from 'react-typical'
 
 const Banner = () => { 
     return (
-        <div className='mt-5 py-5 md:mt-16 md:py-20 banner'>
+        <div className='mt-5 py-5 md:mt-16 md:py-16 banner relative'>
+           <div className='circle1'></div>
             <div className='flex flex-col-reverse md:flex-row md:flex py-20 px-5 primary_color_text 
             justify-between items-center '>
-                       <div className=' md:flex gap-5 items-center justify-between'>
-                       <ul className='flex lg:flex-col md:fixed left-0 justify-around'>
-                        <li className='border-2 h-[50px] mt-2 w-[50px]  bg-white  border-[#F57F03] rounded-full p-2'>
-                               
-                                <Link target='_blank' to={"https://wa.link/nas487"}>
-                                <img  src={whatsapp}/>
+                
+                        <div className='md:w-[900px] md:flex gap-5 items-center justify-between'>
+                        <ul className='flex lg:flex-col md:fixed left-0 justify-around'>
+                            <li className='border-2 h-[50px] mt-2 w-[50px]  bg-white  border-[#F57F03] rounded-full p-2'>
+                                
+                                    <Link target='_blank' to={"https://wa.link/nas487"}>
+                                    <img  src={whatsapp}/>
+                                    </Link>
+                                </li>
+                                <li className='border-2 h-[50px] mt-2 w-[50px]  bg-white   border-[#F57F03] rounded-full p-2'>
+                                <Link target='_blank' to={"https://www.linkedin.com/in/osman84/"}>
+                                <img src={linkedin}/>
                                 </Link>
-                            </li>
-                            <li className='border-2 h-[50px] mt-2 w-[50px]  bg-white   border-[#F57F03] rounded-full p-2'>
-                               <Link target='_blank' to={"https://www.linkedin.com/in/osman84/"}>
-                               <img src={linkedin}/>
-                               </Link>
-                               
-                            </li>
-                            <li className='border-2 h-[50px] mt-2 w-[50px] 
-                             bg-white border-[#F57F03]
-                              rounded-full p-2'>
-                                  <Link target='_blank' to={"https://github.com/osmangoni20"}>
-                                  <img src={github}/>
-                               </Link>
-                              
-                            </li>
-                            <li className='border-2 h-[50px] mt-2 w-[50px]  bg-white border-[#F57F03] rounded-full p-2'>
-                              
-                                <Link target='_blank' to={"https://x.com/osmangoni0827"}>
-                                <img src={twitter}/>
-                               </Link>
-                            </li>
-                            <li className='border-2 h-[50px] mt-2 w-[50px]  bg-white border-[#F57F03] rounded-full p-2'>
-                               
-                                <Link target='_blank' to={"https://www.instagram.com/"}>
-                                <img src={instagram}/>
-                               </Link>
-                            </li>
-                           
-                        </ul>
-                        <div className='md:ml-20 md:text-left text-center'>
-                        <h1 className='py-50'>
-                            <span className='block py-3'>I'm</span>
-                            <span className=' secondary_color_text '>Mohammad Osman Goni</span>
+                                
+                                </li>
+                                <li className='border-2 h-[50px] mt-2 w-[50px] 
+                                bg-white border-[#F57F03]
+                                rounded-full p-2'>
+                                    <Link target='_blank' to={"https://github.com/osmangoni20"}>
+                                    <img src={github}/>
+                                </Link>
+                                
+                                </li>
+                                <li className='border-2 h-[50px] mt-2 w-[50px]  bg-white border-[#F57F03] rounded-full p-2'>
+                                
+                                    <Link target='_blank' to={"https://x.com/osmangoni0827"}>
+                                    <img src={twitter}/>
+                                </Link>
+                                </li>
+                                <li className='border-2 h-[50px] mt-2 w-[50px]  bg-white border-[#F57F03] rounded-full p-2'>
+                                
+                                    <Link target='_blank' to={"https://www.instagram.com/"}>
+                                    <img src={instagram}/>
+                                </Link>
+                                </li>
+                            
+                            </ul>
+                            <div className='md:ml-20 md:text-left text-center'>
+                            <h1 className='py-50'>
+                                <span className='block py-3'>I'm</span>
+                                <span className=' secondary_color_text '>Mohammad Osman Goni</span>
 
-                        </h1>
-                        <h2>
-                            <Typical
-                                loop={Infinity}
-                                wrapper="b"
-                                Delay ms={500}
-                                className="text-2xl"
-                                steps={[
-                                    'Web Developer',
-                                    2000,
-                                    'Frontend Developer',
-                                    2000,
-                                    'Full Stack Developer',
-                                    2000,
-                                    'React Developer',
-                                    2000,
-                                    'Javascript Programmer',
-                                    2000
-                                ]}
+                            </h1>
+                            <h2>
+                                <Typical
+                                    loop={Infinity}
+                                    wrapper="b"
+                                    Delay ms={500}
+                                    className="text-2xl"
+                                    steps={[
+                                        'Web Developer',
+                                        2000,
+                                        'Frontend Developer',
+                                        2000,
+                                        'Full Stack Developer',
+                                        2000,
+                                        'React Developer',
+                                        2000,
+                                        'Javascript Programmer',
+                                        2000
+                                    ]}
 
-                            />
-                        </h2>
-    
-                        <button className=' btn_outline my-3 '>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
-</svg>
+                                />
+                            </h2>
+        <p className="text-justify text-xl py-5">
+            I'm a passionateFull Stack MERN developer.My core skills are based on
+            JavaScript and I love to do most of the programme using JavaScript and
+            TypeScript. I love to make the web more open to the world.
+            </p>
+                            <button className=' btn_outline my-3 sticky'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+    </svg>
 
-                            <Link to={"https://drive.google.com/uc?export=download&id=1jbKqWxMbkw9Bn3tEtNBrUzCQzlceqoSi"}>
-                                Download Resume
-                            </Link>
-                        </button>
+                                <Link to={"https://drive.google.com/uc?export=download&id=1jbKqWxMbkw9Bn3tEtNBrUzCQzlceqoSi"}>
+                                    Download Resume
+                                </Link>
+                            </button>
+                            </div>
                         </div>
-                       </div>
 
-                       <div>
+                       <div className=''>
                         <figure>
-                            <img className=' border-2 h-[300px] w-[300px]
+                            <img className=' profile_image border-2 h-[300px] w-[300px]
                             secondary_color_border rounded-full ' src={profile_image} alt='Profile Image'/>
                         </figure>
                        </div>
